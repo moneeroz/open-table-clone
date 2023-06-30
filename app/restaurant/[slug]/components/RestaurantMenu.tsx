fa05c2822ffb27c94d1152b642b3983ff8695dcc
Moneer <moneer.alromimah@gmail.com>
2023-06-30 17:59:07 -0600

    add restaurant components

--- app/restaurant/[slug]/components/RestaurantMenu.tsx
+++ app/restaurant/[slug]/components/RestaurantMenu.tsx
@@ -1,15 +1,24 @@
+import { Item } from "@prisma/client";
 import MenuCard from "./MenuCard";
 
-const RestaurantMenu = () => {
+const RestaurantMenu = ({ menu }: { menu: Item[] }) => {
   return (
     <main className="bg-white mt-5">
       <div>
         <div className="mt-4 pb-1 mb-1">
           <h1 className="font-bold text-4xl">Menu</h1>
         </div>
-        <div className="flex flex-wrap justify-between">
-          <MenuCard />
-        </div>
+        {menu.length ? (
+          <div className="flex flex-wrap justify-between">
+            {menu.map((item) => (
+              <MenuCard key={item?.id} item={item} />
+            ))}
+          </div>
+        ) : (
+          <div className="flex flex-wrap justify-between">
+            <p>This restaurant does not have an online menu</p>
+          </div>
+        )}
       </div>
     </main>
   );
