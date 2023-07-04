d39e10b879b676cbd3e3da98cf2d56a98078ebdd
Moneer <moneer.alromimah@gmail.com>
2023-07-03 19:46:51 -0600

    add bycrpt to hash the password and jose for JWT

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,37 +1,40 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import validator from "validator";
+import bcrypt from "bcrypt";
+import * as jose from "jose";
 
 const prisma = new PrismaClient();
 
 export const POST = async (req: NextRequest, res: NextResponse) => {
   const body = await req.json();
+  const { firstName, lastName, email, phone, city, password } = body;
 
   const errors: string[] = [];
 
   const validationSchema = [
     {
-      valid: validator.isLength(body.firstName, { min: 1, max: 15 }),
+      valid: validator.isLength(firstName, { min: 1, max: 15 }),
       errorMessage: "First name is invalid",
     },
     {
-      valid: validator.isLength(body.lastName, { min: 1, max: 15 }),
+      valid: validator.isLength(lastName, { min: 1, max: 15 }),
       errorMessage: "Last name is invalid",
     },
     {
-      valid: validator.isEmail(body.email),
+      valid: validator.isEmail(email),
       errorMessage: "Email invalid",
     },
     {
-      valid: validator.isMobilePhone(body.phone),
+      valid: validator.isMobilePhone(phone),
       errorMessage: "Phone number is invalid",
     },
     {
-      valid: validator.isLength(body.city, { min: 1, max: 15 }),
+      valid: validator.isLength(city, { min: 1, max: 15 }),
       errorMessage: "City is invalid",
     },
     {
-      valid: validator.isStrongPassword(body.password),
+      valid: validator.isStrongPassword(password),
       errorMessage: "Password is not strong",
     },
   ];
@@ -43,13 +46,12 @@ export const POST = async (req: NextRequest, res: NextResponse) => {
   });
 
   if (errors.length) {
-    // return new Response(errors[0], { status: 200 });
     return NextResponse.json({ errorMessage: errors[0] }, { status: 400 });
   }
 
   const userWithEmail = await prisma.user.findUnique({
     where: {
-      email: body.email,
+      email,
     },
   });
 
@@ -60,7 +62,27 @@ export const POST = async (req: NextRequest, res: NextResponse) => {
     );
   }
 
-  // return new Response(body, { status: 200 });
+  const salt: number = 10;
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-  return NextResponse.json(body, { status: 200 });
+  const user = await prisma.user.create({
+    data: {
+      first_name: firstName,
+      last_name: lastName,
+      password: hashedPassword,
+      city,
+      phone,
+      email,
+    },
+  });
+
+  const alg = "HS256";
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+  const token = await new jose.SignJWT({ email: user.email })
+    .setProtectedHeader({ alg })
+    .setExpirationTime("24h")
+    .sign(secret);
+
+  return NextResponse.json({ token }, { status: 200 });
 };
