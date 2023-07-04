fc8679ad8a7b5ec4c2975b00255501d0ed382676
Moneer <moneer.alromimah@gmail.com>
2023-07-03 21:10:13 -0600

    update

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -7,82 +7,90 @@ import * as jose from "jose";
 const prisma = new PrismaClient();
 
 export const POST = async (req: NextRequest, res: NextResponse) => {
-  const body = await req.json();
-  const { firstName, lastName, email, phone, city, password } = body;
+  try {
+    const body = await req.json();
+    const { firstName, lastName, email, phone, city, password } = body;
 
-  const errors: string[] = [];
+    const errors: string[] = [];
 
-  const validationSchema = [
-    {
-      valid: validator.isLength(firstName, { min: 1, max: 15 }),
-      errorMessage: "First name is invalid",
-    },
-    {
-      valid: validator.isLength(lastName, { min: 1, max: 15 }),
-      errorMessage: "Last name is invalid",
-    },
-    {
-      valid: validator.isEmail(email),
-      errorMessage: "Email invalid",
-    },
-    {
-      valid: validator.isMobilePhone(phone),
-      errorMessage: "Phone number is invalid",
-    },
-    {
-      valid: validator.isLength(city, { min: 1, max: 15 }),
-      errorMessage: "City is invalid",
-    },
-    {
-      valid: validator.isStrongPassword(password),
-      errorMessage: "Password is not strong",
-    },
-  ];
+    const validationSchema = [
+      {
+        valid: validator.isLength(firstName, { min: 1, max: 15 }),
+        errorMessage: "First name is invalid",
+      },
+      {
+        valid: validator.isLength(lastName, { min: 1, max: 15 }),
+        errorMessage: "Last name is invalid",
+      },
+      {
+        valid: validator.isEmail(email),
+        errorMessage: "Email is invalid",
+      },
+      {
+        valid: validator.isMobilePhone(phone),
+        errorMessage: "Phone number is invalid",
+      },
+      {
+        valid: validator.isLength(city, { min: 1, max: 15 }),
+        errorMessage: "City is invalid",
+      },
+      {
+        valid: validator.isStrongPassword(password),
+        errorMessage: "Password is not strong",
+      },
+    ];
 
-  validationSchema.forEach((check) => {
-    if (!check.valid) {
-      errors.push(check.errorMessage);
+    validationSchema.forEach((check) => {
+      if (!check.valid) {
+        errors.push(check.errorMessage);
+      }
+    });
+
+    if (errors.length) {
+      return NextResponse.json({ errorMessage: errors[0] }, { status: 400 });
     }
-  });
 
-  if (errors.length) {
-    return NextResponse.json({ errorMessage: errors[0] }, { status: 400 });
-  }
+    const userWithEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
 
-  const userWithEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
+    if (userWithEmail) {
+      return NextResponse.json(
+        { errorMessage: "This email is already linked to an account" },
+        { status: 400 },
+      );
+    }
 
-  if (userWithEmail) {
+    const salt: number = 10;
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await prisma.user.create({
+      data: {
+        first_name: firstName,
+        last_name: lastName,
+        password: hashedPassword,
+        city,
+        phone,
+        email,
+      },
+    });
+
+    const alg = "HS256";
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+    const token = await new jose.SignJWT({ email: user.email })
+      .setProtectedHeader({ alg })
+      .setExpirationTime("24h")
+      .sign(secret);
+
+    return NextResponse.json({ token }, { status: 200 });
+  } catch (error) {
+    console.error("An error occurred:", error);
     return NextResponse.json(
-      { errorMessage: "This email is already linked to an account" },
-      { status: 400 },
+      { errorMessage: "Internal server error" },
+      { status: 500 },
     );
   }
-
-  const salt: number = 10;
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  const user = await prisma.user.create({
-    data: {
-      first_name: firstName,
-      last_name: lastName,
-      password: hashedPassword,
-      city,
-      phone,
-      email,
-    },
-  });
-
-  const alg = "HS256";
-  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-
-  const token = await new jose.SignJWT({ email: user.email })
-    .setProtectedHeader({ alg })
-    .setExpirationTime("24h")
-    .sign(secret);
-
-  return NextResponse.json({ token }, { status: 200 });
 };
