3015efa0b87d6de0ddd4a8ce3ce25b532a2032d4
Moneer <moneer.alromimah@gmail.com>
2023-07-10 23:51:26 -0600

    add useAvailabilities hook

---
+++ hooks/useAvailabilities.ts
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const useAvailabilities = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState<
+    { time: string; available: boolean }[] | null
+  >(null);
+
+  const fetchAvailabilities = async ({
+    slug,
+    partySize,
+    day,
+    time,
+  }: {
+    slug: string;
+    partySize: number;
+    day: string;
+    time: string;
+  }) => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/restaurant/${slug}/availability?day=${day}&time=${time}&partySize=${partySize}`,
+        {
+          method: "GET",
+        },
+      );
+      setLoading(false);
+
+      const data = await response.json();
+
+      setData(data);
+    } catch (error: any) {
+      setLoading(false);
+      setError(error.errorMessage);
+    }
+  };
+
+  return { loading, error, data, fetchAvailabilities };
+};
+
+export default useAvailabilities;
