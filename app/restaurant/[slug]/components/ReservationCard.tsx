5212369b780ddf26a2212ee786c962d419c4890c
Moneer <moneer.alromimah@gmail.com>
2023-07-10 23:52:11 -0600

    implement availability

--- app/restaurant/[slug]/components/ReservationCard.tsx
+++ app/restaurant/[slug]/components/ReservationCard.tsx
@@ -1,23 +1,47 @@
 "use client";
 
-import { partySize, times } from "@/data";
+import { partySize as PartySize, times } from "@/data";
+import useAvailabilities from "@/hooks/useAvailabilities";
+import { Time, convertToDisplayTime } from "@/utils/convertToDisplayTime";
+import { CircularProgress } from "@mui/material";
+import Link from "next/link";
 import { useState } from "react";
 import DatePicker from "react-datepicker";
 
 const ReservationCard = ({
   openTime,
   closeTime,
+  slug,
 }: {
   openTime: string;
   closeTime: string;
+  slug: string;
 }) => {
+  const { data, error, loading, fetchAvailabilities } = useAvailabilities();
   const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+  const [time, setTime] = useState(openTime);
+  const [partySize, setPartySize] = useState(1);
+  const [day, setDay] = useState(new Date().toISOString().split("T")[0]);
+
+  console.log(data);
 
   const handleDateChange = (date: Date | null) => {
     if (!date) {
       return setSelectedDate(null);
     }
+
+    setDay(date.toISOString().split("T")[0]);
+
     return setSelectedDate(date);
+  };
+
+  const handleClick = () => {
+    fetchAvailabilities({
+      slug,
+      day,
+      time,
+      partySize,
+    });
   };
 
   const filterTimesByOpenWindow = () => {
@@ -48,8 +72,10 @@ const ReservationCard = ({
           name=""
           className="py-3 border-b font-light bg-transparent"
           id=""
+          value={partySize}
+          onChange={(e) => setPartySize(Number(e.target.value))}
         >
-          {partySize.map((size) => (
+          {PartySize.map((size) => (
             <option value={size.value} key={size.label}>
               {size.label}
             </option>
@@ -73,6 +99,8 @@ const ReservationCard = ({
             name=""
             id=""
             className="py-[14.3px] border-b font-light bg-transparent"
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
           >
             {filterTimesByOpenWindow().map((time) => (
               <option value={time.time} key={time.displayTime}>
@@ -83,10 +111,39 @@ const ReservationCard = ({
         </div>
       </div>
       <div className="mt-5">
-        <button className="bg-red-600 rounded w-full px-4 text-white font-bold h-16">
-          Find a Time
+        <button
+          className="bg-red-600 rounded w-full px-4 text-white font-bold h-16"
+          onClick={handleClick}
+          disabled={loading}
+        >
+          {loading ? <CircularProgress color="inherit" /> : "Find a Time"}
         </button>
       </div>
+      {data && data.length ? (
+        <div className="mt-4">
+          <p className="text-reg">Select a Time</p>
+          <div className="flex flex-wrap mt-2">
+            {data.map((time) => {
+              return time.available ? (
+                <Link
+                  href={`/reserve/${slug}?date=${day}T${time.time}&partySize=${partySize}`}
+                  className="bg-red-600 cursor-pointer p-2 w-24 text-center text-white rounded mr-3 mb-3"
+                  key={time.time}
+                >
+                  <p className="text-sm font-bold">
+                    {convertToDisplayTime(time.time as Time)}
+                  </p>
+                </Link>
+              ) : (
+                <p
+                  className="bg-gray-300 p-4 w-24 rounded mr-3 mb-3"
+                  key={time.time}
+                ></p>
+              );
+            })}
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
